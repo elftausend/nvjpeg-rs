bb015c5b89b6e65773213addb82d0cf3d92e8d6d
elftausend <76885970+elftausend@users.noreply.github.com>
2023-07-04 17:44:54 +0200

    correlation cuda

--- nvjpeg-sys/build.rs
+++ nvjpeg-sys/build.rs
@@ -1,78 +1,7 @@
-use std::path::{Path, PathBuf};
-
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
     if std::env::var("DOCS_RS").is_ok() {
         return;
     }
-
-    // not required if the main branch version of custos is used
-    link_cuda();
-}
-
-// https://github.com/coreylowman/cudarc/blob/main/build.rs
-fn link_cuda() {
-    println!("cargo:rerun-if-env-changed=CUDA_ROOT");
-    println!("cargo:rerun-if-env-changed=CUDA_PATH");
-    println!("cargo:rerun-if-env-changed=CUDA_TOOLKIT_ROOT_DIR");
-
-    let candidates: Vec<PathBuf> = root_candidates().collect();
-
-    let toolkit_root = root_candidates()
-        .find(|path| path.join("include").join("cuda.h").is_file())
-        .unwrap_or_else(|| {
-            panic!(
-                "Unable to find `include/cuda.h` under any of: {candidates:?}. Set the `CUDA_ROOT` environment variable to `$CUDA_ROOT/include/cuda.h` to override path.",
-            )
-        });
-
-    for path in lib_candidates(&toolkit_root) {
-        println!("cargo:rustc-link-search=native={}", path.display());
-    }
-
-    println!("cargo:rustc-link-lib=dylib=nvjpeg");
-}
-
-fn root_candidates() -> impl Iterator<Item = PathBuf> {
-    let env_vars = [
-        "CUDA_PATH",
-        "CUDA_ROOT",
-        "CUDA_TOOLKIT_ROOT_DIR",
-        "CUDNN_LIB",
-    ];
-    let env_vars = env_vars
-        .into_iter()
-        .map(std::env::var)
-        .filter_map(Result::ok);
-
-    let roots = [
-        "/usr",
-        "/usr/local/cuda",
-        "/opt/cuda",
-        "/usr/lib/cuda",
-        "C:/Program Files/NVIDIA GPU Computing Toolkit",
-        "C:/CUDA",
-    ];
-    let roots = roots.into_iter().map(Into::into);
-    env_vars.chain(roots).map(Into::<PathBuf>::into)
-}
-
-fn lib_candidates(root: &Path) -> Vec<PathBuf> {
-    [
-        "lib",
-        "lib/x64",
-        "lib/Win32",
-        "lib/x86_64",
-        "lib/x86_64-linux-gnu",
-        "lib64",
-        "lib64/stubs",
-        "targets/x86_64-linux",
-        "targets/x86_64-linux/lib",
-        "targets/x86_64-linux/lib/stubs",
-    ]
-    .iter()
-    .map(|&p| root.join(p))
-    .filter(|p| p.is_dir())
-    .collect()
 }
--- nvjpeg-sys/examples/apply_filter.rs
+++ nvjpeg-sys/examples/apply_filter.rs
@@ -1,6 +1,11 @@
-use std::{io::Write, ptr::null_mut, ops::Mul};
+use std::{io::Write, mem::size_of, ops::Mul, ptr::null_mut};
 
-use custos::{Buffer, CUDA, prelude::Number};
+use custos::{
+    buf,
+    cuda::{api::culaunch_kernel, fn_cache, launch_kernel},
+    prelude::{launch_kernel1d, CUBuffer, Number},
+    Buffer, CDatatype, Device, CUDA,
+};
 use nvjpeg_sys::{
     check, nvjpegChromaSubsampling_t, nvjpegCreateSimple, nvjpegDecode, nvjpegDestroy,
     nvjpegGetImageInfo, nvjpegHandle_t, nvjpegImage_t, nvjpegJpegStateCreate,
@@ -85,24 +90,55 @@ unsafe fn decode_raw_jpeg(
     let channel1 = channel1.read();
     let channel2 = channel2.read();
 
-
-    let filter_rows = 17;
-    let filter_cols = 17;
-    let filter = vec![1./(filter_rows * filter_cols) as f32; filter_rows * filter_cols];
+    let filter_rows = 300;
+    let filter_cols = 300;
+    let filter = vec![1. / (filter_rows * filter_cols) as f32; filter_rows * filter_cols];
 
     let mut channel0_out = vec![0.; channel0.len()];
-    correlate_fully(&channel0.iter().map(|x| *x as f32).collect::<Vec<_>>(), &filter, &mut channel0_out, heights[0] as usize, widths[0] as usize, filter_rows, filter_cols);
+    correlate_fully(
+        &channel0.iter().map(|x| *x as f32).collect::<Vec<_>>(),
+        &filter,
+        &mut channel0_out,
+        heights[0] as usize,
+        widths[0] as usize,
+        filter_rows,
+        filter_cols,
+    );
 
     let mut channel1_out = vec![0.; channel1.len()];
-    correlate_fully(&channel1.iter().map(|x| *x as f32).collect::<Vec<_>>(), &filter, &mut channel1_out, heights[0] as usize, widths[0] as usize, filter_rows, filter_cols);
+    correlate_fully(
+        &channel1.iter().map(|x| *x as f32).collect::<Vec<_>>(),
+        &filter,
+        &mut channel1_out,
+        heights[0] as usize,
+        widths[0] as usize,
+        filter_rows,
+        filter_cols,
+    );
 
     let mut channel2_out = vec![0.; channel2.len()];
-    correlate_fully(&channel2.iter().map(|x| *x as f32).collect::<Vec<_>>(), &filter, &mut channel2_out, heights[0] as usize, widths[0] as usize, filter_rows, filter_cols);
-    
-    let channel0_out = channel0_out.into_iter().map(|x| x as u8).collect::<Vec<_>>();
-    let channel1_out = channel1_out.into_iter().map(|x| x as u8).collect::<Vec<_>>();
-    let channel2_out = channel2_out.into_iter().map(|x| x as u8).collect::<Vec<_>>();
+    correlate_fully(
+        &channel2.iter().map(|x| *x as f32).collect::<Vec<_>>(),
+        &filter,
+        &mut channel2_out,
+        heights[0] as usize,
+        widths[0] as usize,
+        filter_rows,
+        filter_cols,
+    );
 
+    let channel0_out = channel0_out
+        .into_iter()
+        .map(|x| x as u8)
+        .collect::<Vec<_>>();
+    let channel1_out = channel1_out
+        .into_iter()
+        .map(|x| x as u8)
+        .collect::<Vec<_>>();
+    let channel2_out = channel2_out
+        .into_iter()
+        .map(|x| x as u8)
+        .collect::<Vec<_>>();
 
     let file = std::fs::File::create("cat_798x532.ppm")?;
     let mut writer = std::io::BufWriter::new(file);
@@ -123,22 +159,248 @@ unsafe fn decode_raw_jpeg(
     writer.flush()?;
 
     // free
-
+    /*
     let status = nvjpegJpegStateDestroy(jpeg_state);
     check!(status, "Could not free jpeg state. ");
 
     let status = nvjpegDestroy(handle);
-    check!(status, "Could not free nvjpeg handle. ");
+    check!(status, "Could not free nvjpeg handle. ");*/
 
     Ok(Image::default())
 }
 
-pub fn add_padding<T: Number>(inputs: &[T], inp_rows: usize, inp_cols: usize, x_padding: usize, y_padding: usize) -> Vec<T> {
-    let mut padded_inputs = vec![T::zero(); (inp_rows + y_padding * 2) * (inp_cols + x_padding * 2)];
+pub fn correlate_cu_padded<T: Number + CDatatype>(
+    input: &CUBuffer<T>,
+    filter: &CUBuffer<T>,
+    out: &mut CUBuffer<T>,
+    inp_rows: usize,
+    inp_cols: usize,
+    filter_rows: usize,
+    filter_cols: usize,
+) {
+    let x_padding = filter_cols - 1;
+    let y_padding = filter_rows - 1;
+
+    let inp_rows2 = inp_rows + y_padding * 2;
+    let inp_cols2 = inp_cols + x_padding * 2;
+
+    let src = format!(
+        r#"
+        extern "C" __global__ void correlate({dtype}* input, {dtype}* filter, {dtype}* out, int inp_rows, int inp_cols, int unpad_rows, int unpad_cols, int filter_rows, int filter_cols) {{
+            int idx = blockDim.x * blockIdx.x + threadIdx.x;
+
+            int xPadding = filter_cols - 1;
+            int yPadding = filter_rows - 1;
+
+            int moveDown = blockIdx.x / (inp_cols-filter_cols + 1);
+            int moveRight = blockIdx.x % (inp_cols-filter_cols + 1);
+            int startOfBlock = moveDown * inp_cols + moveRight;
+            int next = startOfBlock + (threadIdx.x / filter_cols) * inp_cols + (threadIdx.x % filter_cols);
+
+            (yPadding + unpad_rows) * inp_cols
+
+            if (next < 0) {{
+                return;
+            }}
+
+            //(yPadding + ) * inp_cols 
+
+            if (next >= inp_rows * inp_cols || next < 0) {{
+              //  return;
+            }}
+
+            //__shared__ {dtype} res[filter_rows * filter_cols];
+            extern __shared__ {dtype} res[];
+
+            res[threadIdx.x] = input[next] * filter[threadIdx.x];
+
+            __syncthreads();
+
+            // sum res and write to out
+            if (threadIdx.x == 0) {{
+                {dtype} sum = 0;
+                for (int i = 0; i < filter_rows * filter_cols; i++) {{
+                    sum += res[i];
+                }}
+                out[blockIdx.x] = sum;
+            }}
+            //printf("block: %d, idx: %d, val: %f  \n", blockIdx.x, next, input[next]);
+            //printf("shared val: %f  \n", res[0]);
+        }} 
+    "#,
+        dtype = T::as_c_type_str()
+    );
+
+    launch_kernel(
+        input.device(),
+        [
+            ((inp_rows2 - filter_rows + 1) * (inp_cols2 - filter_cols + 1)) as u32,
+            1,
+            1,
+        ],
+        [(filter_cols * filter_rows) as u32, 1, 1],
+        (filter_cols * filter_rows * size_of::<T>()) as u32,
+        &src,
+        "correlate",
+        &[
+            input,
+            filter,
+            out,
+            &inp_rows2,
+            &inp_cols2,
+            &inp_rows,
+            &inp_cols,        
+            &filter_rows,
+            &filter_cols,
+        ],
+    )
+    .unwrap();
+}
+
+#[test]
+fn test_correlate_cu_padded() {
+
+    #[rustfmt::skip]
+    let data = buf![
+        1., 2., 3., 4., 
+        5., 6., 7., 8., 
+        9., 10., 11., 12., 
+        13., 14., 15., 16., 
+        17., 18., 19., 20.
+    ]
+    .to_gpu();
+
+    let filter = buf![1.; 9].to_gpu();
+    let mut out = buf![0.; data.len()].to_gpu();
+
+    correlate_cu_padded(&data, &filter, &mut out, 5, 4, 3, 3);
+    println!("out: {out:?}");
+}
+
+pub fn correlate_cu<T: Number + CDatatype>(
+    input: &CUBuffer<T>,
+    filter: &CUBuffer<T>,
+    out: &mut CUBuffer<T>,
+    inp_rows: usize,
+    inp_cols: usize,
+    filter_rows: usize,
+    filter_cols: usize,
+) {
+    let src = format!(
+        r#"
+        extern "C" __global__ void correlate({dtype}* input, {dtype}* filter, {dtype}* out, int inp_rows, int inp_cols, int filter_rows, int filter_cols) {{
+            int idx = blockDim.x * blockIdx.x + threadIdx.x;
+
+            int moveDown = blockIdx.x / (inp_cols-filter_cols + 1);
+            //int moveRight = blockIdx.x % (inp_cols - filter_cols + 1);
+            int moveRight = blockIdx.x % (inp_cols-filter_cols + 1);
+            int startOfBlock = moveDown * inp_cols + moveRight;
+            int next = startOfBlock + (threadIdx.x / filter_cols) * inp_cols + (threadIdx.x % filter_cols);
+
+            //__shared__ {dtype} res[filter_rows * filter_cols];
+            extern __shared__ {dtype} res[];
+
+            res[threadIdx.x] = input[next] * filter[threadIdx.x];
+
+            __syncthreads();
+
+            // sum res and write to out
+            if (threadIdx.x == 0) {{
+                {dtype} sum = 0;
+                for (int i = 0; i < filter_rows * filter_cols; i++) {{
+                    sum += res[i];
+                }}
+                out[blockIdx.x] = sum;
+            }}
+            //printf("block: %d, idx: %d, val: %f  \n", blockIdx.x, next, input[next]);
+            //printf("shared val: %f  \n", res[0]);
+        }} 
+    "#,
+        dtype = T::as_c_type_str()
+    );
+
+    launch_kernel(
+        input.device(),
+        [
+            ((inp_rows - filter_rows + 1) * (inp_cols - filter_cols + 1)) as u32,
+            1,
+            1,
+        ],
+        [(filter_cols * filter_rows) as u32, 1, 1],
+        (filter_cols * filter_rows * size_of::<T>()) as u32,
+        &src,
+        "correlate",
+        &[
+            input,
+            filter,
+            out,
+            &inp_rows,
+            &inp_cols,
+            &filter_rows,
+            &filter_cols,
+        ],
+    )
+    .unwrap();
+}
+
+#[test]
+fn test_correlate_cu() {
+    let data = buf![
+        1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12., 13., 14., 15., 16., 17., 18., 19., 20.
+    ]
+    .to_gpu();
+
+    let filter = buf![1.; 9].to_gpu();
+
+    let mut out = buf![0.; data.len()].to_gpu();
+
+    correlate_cu(&data, &filter, &mut out, 5, 4, 3, 3);
+    println!("out: {out:?}");
+}
+
+#[test]
+fn test_correlate_cu_larger() {
+
+    let rows = 5080;
+    let cols = 520;
+
+    let data = buf![
+        1.4; rows * cols
+    ]
+    .to_gpu();
+
+
+    let filter_rows = 32;
+    let filter_cols = filter_rows;
+
+    let filter = buf![1.; filter_rows * filter_cols].to_gpu();
+
+    let mut out  = buf![0.; (rows - filter_rows + 1) * (cols - filter_cols +1)].to_gpu();
+    // correlate_cu(&data, &filter, &mut out, rows, cols, filter_rows, filter_cols);
+    // out.device().stream().sync().unwrap();
+    let start = std::time::Instant::now();
+    correlate_cu(&data, &filter, &mut out, rows, cols, filter_rows, filter_cols);
+    out.device().stream().sync().unwrap();
+    println!("elapsed: {:?}", start.elapsed());
+    //println!("out: {out:?}");
+}
+
+pub fn add_padding<T: Number>(
+    inputs: &[T],
+    inp_rows: usize,
+    inp_cols: usize,
+    x_padding: usize,
+    y_padding: usize,
+) -> Vec<T> {
+    let mut padded_inputs =
+        vec![T::zero(); (inp_rows + y_padding * 2) * (inp_cols + x_padding * 2)];
 
     for inp_row in 0..inp_rows {
         for inp_col in 0..inp_cols {
-            padded_inputs[y_padding * (inp_cols + 2*x_padding) + x_padding + inp_row * (inp_cols + 2*x_padding) + inp_col] = inputs[inp_row * inp_cols + inp_col];
+            padded_inputs[y_padding * (inp_cols + 2 * x_padding)
+                + x_padding
+                + inp_row * (inp_cols + 2 * x_padding)
+                + inp_col] = inputs[inp_row * inp_cols + inp_col];
         }
     }
     padded_inputs
@@ -163,16 +425,14 @@ pub fn correlate_fully<T: Number + Mul<U, Output = T>, U: Number>(
     // attention: leaves the last padded row, col out
     for move_down in 0..=padded_rows - filter_rows - y_padding {
         for move_right in 0..=padded_cols - filter_cols - x_padding {
-
             let mut sum = T::default();
             for idx in 0..filter_rows {
-                
                 let filter_idx = idx * filter_cols;
                 let filter_row = &filter[filter_idx..filter_idx + filter_cols];
 
                 let input_idx = move_down * padded_cols + move_right + idx * padded_cols;
                 let input_row = &padded_inputs[input_idx..input_idx + filter_cols];
-    
+
                 for (filter_row, input_row) in input_row.iter().zip(filter_row) {
                     sum += *filter_row * *input_row;
                 }
@@ -185,41 +445,52 @@ pub fn correlate_fully<T: Number + Mul<U, Output = T>, U: Number>(
 #[test]
 fn test_correlate() {
     let data = [
-        1., 2., 3., 5., 
-        4., 3., 2., 1.,
-        8., 7., 4., 2.,
-        7., 3., 2., 1.,
-        8., 5., 3., 8.
+        1., 2., 3., 5., 4., 3., 2., 1., 8., 7., 4., 2., 7., 3., 2., 1., 8., 5., 3., 8.,
     ];
 
-    let filter = [1./4., 1./4., 1./4., 1./4.];
-    let mut out = [0.; 5*4];
+    let filter = [1. / 4., 1. / 4., 1. / 4., 1. / 4.];
+    let mut out = [0.; 5 * 4];
     correlate_fully(&data, &filter, &mut out, 5, 4, 2, 2);
 
     println!("out: {out:?}");
 
-    let filter = [1./4.; 9];
-    let mut out = [0.; 5*4];
+    let filter = [1. / 4.; 9];
+    let mut out = [0.; 5 * 4];
     correlate_fully(&data, &filter, &mut out, 5, 4, 3, 3);
 
     println!("out: {out:?}");
 }
 
 #[test]
-fn test_add_padding() {
-    let data = [
-        1, 2, 3, 5, 
-        4, 3, 2, 1,
-        8, 7, 4, 2,
-        7, 3, 2, 1,
-        8, 5, 3, 8
+fn test_correlate_larger() {
+    let rows = 5080;
+    let cols = 520;
+
+    let data = buf![
+        1.4; rows * cols
     ];
+
+
+    let filter_rows = 32;
+    let filter_cols = filter_rows;
+
+    let filter = buf![1.; filter_rows * filter_cols];
+
+    let mut out = buf![0.; rows * cols];
+    
+    correlate_fully(&data, &filter, &mut out, rows, cols, filter_rows, filter_cols);
+    println!("out: {out:?}");
+}
+
+#[test]
+fn test_add_padding() {
+    let data = [1, 2, 3, 5, 4, 3, 2, 1, 8, 7, 4, 2, 7, 3, 2, 1, 8, 5, 3, 8];
 
     let padded = add_padding(&data, 5, 4, 2, 2);
 
     for (idx, padded_val) in padded.iter().enumerate() {
         print!("{padded_val}, ");
-        if (idx+1) % (2 + 2 + 4) == 0 {
+        if (idx + 1) % (2 + 2 + 4) == 0 {
             println!()
         }
     }
